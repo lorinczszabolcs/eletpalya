d0f8252a81bbd6d82d9a689859ea2ed185343df9
Lőrincz Szabolcs-Botond <lorincz_szabolcs@yahoo.com>
2018-04-01 22:58:21 +0300

    Major Update, and TODO

---
+++ js/plot10.js
@@ -0,0 +1,63 @@
+function loadJSON(callback, filename) {
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', filename, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+(function() {
+  var d3 = Plotly.d3;
+
+  // Plot1
+  var WIDTH_IN_PERCENT_OF_PARENT = 98,
+      HEIGHT_IN_PERCENT_OF_PARENT = 70;
+
+  var gd3 = d3.select('#plot10')
+      .style({
+        width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+        height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh'
+      });
+
+  var gd = gd3.node();
+
+  loadJSON(function(response) {
+    var actualJSON = JSON.parse(response);
+    var dictionary = {};
+    for (elem in actualJSON)
+    {
+      dictionary[elem] = actualJSON[elem];
+    }
+    var data = Object.keys(dictionary).map(function(key){
+      return dictionary[key];
+    });
+
+    var layout =
+    {
+      title: "<b class='text-center'>Szakterületek csoportosítása tanulmányok szerint<br>(normalizálva)</b>",
+      "titlefont":
+      {
+        "size": 20,
+      },
+      yaxis:
+      {
+        tickformat: '%',
+        range: [0,1]
+      },
+      barmode: 'stack'
+    };
+
+    Plotly.plot(gd, data, layout);
+
+  }, '/data/stud_work_normalized.json');
+
+  window.onresize = function() {
+      Plotly.Plots.resize(gd);
+  };
+})();
